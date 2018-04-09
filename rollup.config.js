6913e79d9a3de9c0ae1149677a8adb1ee3b8b0c0
Vincent Guillou <vin.guillou@gmail.com>
2018-04-10 01:20:09 +0200

    Adding rollup-plugin-serve

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,17 @@
 import babel from 'rollup-plugin-babel';
 import uglify from 'rollup-plugin-uglify';
+import serve from 'rollup-plugin-serve';
 import pkg from './package.json';
 
 const PROD = process.env.NODE_ENV === 'production';
+const DEV = process.env.NODE_ENV === 'development';
 const INPUT = 'src/index.js';
 
 // browser-friendly UMD build
 const configUmd = {
   input: INPUT,
   output: {
-    name: 'flip',
+    name: 'Flip',
     file: PROD ? pkg.mainMin : pkg.main,
     format: 'umd',
     sourcemap: !PROD,
@@ -19,6 +21,12 @@ const configUmd = {
       exclude: 'node_modules/**',
     }),
     PROD && uglify(),
+    DEV &&
+      serve({
+        open: true,
+        contentBase: ['examples', 'dist'],
+        port: 10002,
+      }),
   ],
 };
 
